2649a62d8c21872eb93fc4cae18236b239eed40c
PierricM380 <pierric.marye@wanadoo.fr>
2022-11-03 17:19:41 +0100

    Typed library for title

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* Typing animation from library typed.js */
+var typed = new Typed(".typing", {
+    strings: ["sites web", "applications mobiles"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
